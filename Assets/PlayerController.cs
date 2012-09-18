326a08e3947d74f2a6427502e89cab18b4313d40
Eder <edermtz.04@gmail.com>
2012-09-18 18:28:25 -0500

    Kinect Detecta el Usuario y trackea

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -11,28 +11,103 @@ using OpenNI;
 using NITE;
 
 public class PlayerController : MonoBehaviour {
-	public float MoveSpeed = 10;
-	public float RotateSpeed = 40;
+/*
+ * Iniciar OpenNI
+ */
+	private readonly string XML_CONFIG=@".//OpenNI.xml";
+	private Context context;
+	private ScriptNode scriptNode;
+	private DepthGenerator depth;
+	private UserGenerator userGenerator;
+	private SkeletonCapability skeletonCapability;
+	private PoseDetectionCapability poseDetectionCapability;
+	private string calibPose;
 	
 	
-	//Declaraciones-END
+	private Dictionary <int, Dictionary<SkeletonJoint,SkeletonJointPosition>> joints;
+	private bool shouldRun;
 	//Para inicar
 	void Start () {
 		Debug.Log("Start");
+		this.context=Context.CreateFromXmlFile(XML_CONFIG,out scriptNode);
+		//Al Ejecutar esta linea al salir de probar el nodo sigue trabajando para eso ocupamos el onApplicationQuit
+		this.depth=context.FindExistingNode(NodeType.Depth) as DepthGenerator;
+		if(depth==null){
+			throw new Exception("Nodo de Profundidad no encontrado");
+		}
+		
+		this.userGenerator=new UserGenerator(this.context);
+		this.skeletonCapability=this.userGenerator.SkeletonCapability;
+		this.poseDetectionCapability=this.userGenerator.PoseDetectionCapability;
+		this.calibPose=this.skeletonCapability.CalibrationPose;
+		this.userGenerator.NewUser+=userGenerator_NewUser;
+		this.userGenerator.LostUser+=userGenerator_LostUser;
+		this.poseDetectionCapability.PoseDetected+=poseDetectionCapability_PoseDetected;
+		this.skeletonCapability.CalibrationComplete+=skeletonCapability_CalibrationComplete;
+		this.skeletonCapability.SetSkeletonProfile(SkeletonProfile.All);
+		this.joints=new Dictionary<int,Dictionary<SkeletonJoint,SkeletonJointPosition>>();
+		//Generar
+		this.userGenerator.StartGenerating();
+		this.shouldRun=true;
 	}	       
 	
+	void userGenerator_NewUser(object sender, NewUserEventArgs e){
+          if (this.skeletonCapability.DoesNeedPoseForCalibration){
+				Debug.Log("1");
+            	this.poseDetectionCapability.StartPoseDetection(this.calibPose, e.ID);
+           }else{
+				Debug.Log("2");
+            	this.skeletonCapability.RequestCalibration(e.ID, true);
+            }
+    }
+	
+	void poseDetectionCapability_PoseDetected(object sender, PoseDetectedEventArgs e){
+		Debug.Log("4");
+            this.poseDetectionCapability.StopPoseDetection(e.ID);
+            this.skeletonCapability.RequestCalibration(e.ID, true);
+    }
+	
+	void skeletonCapability_CalibrationComplete(object sender, CalibrationProgressEventArgs e){
+		Debug.Log("5");
+            if (e.Status == CalibrationStatus.OK){
+                this.skeletonCapability.StartTracking(e.ID);
+                this.joints.Add(e.ID, new Dictionary<SkeletonJoint, SkeletonJointPosition>());
+            }else if (e.Status != CalibrationStatus.ManualAbort){
+                if (this.skeletonCapability.DoesNeedPoseForCalibration){
+                    this.poseDetectionCapability.StartPoseDetection(calibPose, e.ID);
+                }else{
+                    this.skeletonCapability.RequestCalibration(e.ID, true);
+                }
+            }
+    }
+	
+	void userGenerator_LostUser(object sender, UserLostEventArgs e){
+		Debug.Log ("3");
+		this.joints.Remove(e.ID);
+	}
 	
 	void Update () {
 		Debug.Log("Update");
-		float MoveForward = Input.GetAxis("Vertical")  * MoveSpeed * Time.deltaTime;
-    	float MoveRotate = Input.GetAxis("Horizontal") * RotateSpeed * Time.deltaTime;
-		
-		// Move the player
-		//Avanzar
-    	transform.Translate(Vector3.forward * MoveForward);
-    	//Rotar
-		transform.Rotate(Vector3.up * MoveRotate);
-		Debug.Log(transform.rotation.x+","+transform.rotation.y+","+transform.rotation.z+","+transform.rotation.w);
-		
+		if(this.shouldRun){
+			try{
+				this.context.WaitOneUpdateAll(this.depth);
+			}catch(Exception){
+				Debug.Log("No paso");
+			}
+			
+			int[] users=this.userGenerator.GetUsers();
+			foreach(int user in users){
+				if(this.skeletonCapability.IsTracking(user)){
+					Debug.Log ("Esta trackeando Usuario #"+user);
+				}
+			}
+		}
 	}
+	
+	void OnApplicationQuit(){
+		Debug.Log("Saliendo de la aplicacion");
+		context.Release();
+	}
+	
+	
 }
