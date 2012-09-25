4570425355b0626fd13f55918aedcceeb0af77fe
Eder <edermtz.04@gmail.com>
2012-09-25 17:50:30 -0500

    antes de intentar 2 objetos

--- Assets/Camara.cs
+++ Assets/Camara.cs
@@ -1,19 +1,13 @@
 using UnityEngine;
 using System.Collections;
-using System;
 using System.Collections.Generic;
-/*
- * Usare los .dll oficiales de Primesense por que los de thinkerer los modifico
- */ 
+using System;
 //OpenNI
 using OpenNI;
 //NITE
 using NITE;
 
-public class PlayerController : MonoBehaviour {
-/*
- * Iniciar OpenNI
- */
+public class Camara : MonoBehaviour {
 	private readonly string XML_CONFIG=@".//OpenNI.xml";
 	private Context context;
 	private ScriptNode scriptNode;
@@ -22,15 +16,8 @@ public class PlayerController : MonoBehaviour {
 	private SkeletonCapability skeletonCapability;
 	private PoseDetectionCapability poseDetectionCapability;
 	private string calibPose;
-	Quaternion rotationInitial;
-	//NITE-HAND
-	private HandsGenerator hands;
-	private GestureGenerator gestures;
-	//NITE-HAND
-	
 	private Dictionary <int, Dictionary<SkeletonJoint,SkeletonJointPosition>> joints;
 	private bool shouldRun;
-	
 	
 	//aaron
 	public float valorPositivoRotacion=1;
@@ -38,73 +25,37 @@ public class PlayerController : MonoBehaviour {
 	public float zInicial=1320;
 	public float xInicial=100;
 	public float yInicial=-30;
-	//Para inicar
-	
-	//----
-	public float xAnterior=0;
-	public float yAnterior=0;
-	public float zAnterior=0;
-	//----
-	
+
+	// Use this for initialization
 	void Start () {
-		Debug.Log("Start");
 		this.context=Context.CreateFromXmlFile(XML_CONFIG,out scriptNode);
 		//Al Ejecutar esta linea al salir de probar el nodo sigue trabajando para eso ocupamos el onApplicationQuit
 		this.depth=context.FindExistingNode(NodeType.Depth) as DepthGenerator;
 		if(depth==null){
 			throw new Exception("Nodo de Profundidad no encontrado");
 		}
+		this.userGenerator=new UserGenerator(this.context);
+		this.skeletonCapability=this.userGenerator.SkeletonCapability;
+		this.poseDetectionCapability=this.userGenerator.PoseDetectionCapability;
+		this.calibPose=this.skeletonCapability.CalibrationPose;
+		//Agregas los handlers
+		this.userGenerator.NewUser+=userGenerator_NewUser;
+		this.userGenerator.LostUser+=userGenerator_LostUser;
+		this.poseDetectionCapability.PoseDetected+=poseDetectionCapability_PoseDetected;
+		this.skeletonCapability.CalibrationComplete+=skeletonCapability_CalibrationComplete;
+		//Activar los joints depende del profile
+		//http://openni.org/docs2/Reference/_xn_types_8h_a294999eabe6eeab319a61d3d0093b174.html#a294999eabe6eeab319a61d3d0093b174
+		this.skeletonCapability.SetSkeletonProfile(SkeletonProfile.All);
+		this.joints=new Dictionary<int,Dictionary<SkeletonJoint,SkeletonJointPosition>>();
 		
-		this.hands = context.FindExistingNode (NodeType.Hands) as HandsGenerator;
-		if (this.hands == null) {
-			Debug.LogError ("Viewer must have a hands node!");
-		}
-		this.gestures = context.FindExistingNode (NodeType.Gesture) as GestureGenerator;
-		if (this.gestures == null) {
-			Debug.LogError ("Viewer must have a gestures node!");
-		}
-		
-		
-		IniciaUser();
-		IniciaHand();
-		
-		//Generar
 		this.userGenerator.StartGenerating();
 		this.shouldRun=true;
+	
 	}
 	
-	
-	void userGenerator_NewUser(object sender, NewUserEventArgs e){
-          if (this.skeletonCapability.DoesNeedPoseForCalibration){
-            	this.poseDetectionCapability.StartPoseDetection(this.calibPose, e.ID);
-           }else{
-            	this.skeletonCapability.RequestCalibration(e.ID, true);
-            }
-    }
-	
-	void poseDetectionCapability_PoseDetected(object sender, PoseDetectedEventArgs e){
-            this.poseDetectionCapability.StopPoseDetection(e.ID);
-            this.skeletonCapability.RequestCalibration(e.ID, true);
-    }
-	
-	void skeletonCapability_CalibrationComplete(object sender, CalibrationProgressEventArgs e){
-            if (e.Status == CalibrationStatus.OK){
-                this.skeletonCapability.StartTracking(e.ID);
-                this.joints.Add(e.ID, new Dictionary<SkeletonJoint, SkeletonJointPosition>());
-            }else if (e.Status != CalibrationStatus.ManualAbort){
-                if (this.skeletonCapability.DoesNeedPoseForCalibration){
-                    this.poseDetectionCapability.StartPoseDetection(calibPose, e.ID);
-                }else{
-                    this.skeletonCapability.RequestCalibration(e.ID, true);
-                }
-            }
-    }
-	
-	void userGenerator_LostUser(object sender, UserLostEventArgs e){
-		this.joints.Remove(e.ID);
-	}
-	
+	// Update is called once per frame
 	void Update () {
+		
 		//Debug.Log("Update");
 		if(this.shouldRun){
 			try{
@@ -157,24 +108,13 @@ public class PlayerController : MonoBehaviour {
 						
 						transform.rotation = Quaternion.Lerp(fromX, toX, Time.time * .001f);
 						//transform.Rotate (new Vector3((transform.rotation.x *-1),0f,0f));				
-					}
-					//transform.rotation=q;
-					//Traslacion
-					SkeletonJointPosition posicion=this.skeletonCapability.GetSkeletonJointPosition(user,SkeletonJoint.Torso);
-					if(posicion.Position.Z<1170F){
-						//transform.Translate(Vector3.forward*10f*Time.deltaTime);
-						transform.Translate(Vector3.forward * 0.107832f*5 *Time.deltaTime);
-					}else if(posicion.Position.Z>1470F){
-						//transform.Translate(Vector3.back*10f*Time.deltaTime);
-					}
-					
-					
-					
-					
+					}	
 				}
 			}
 		}
+	
 	}
+	
 	
 	void OnApplicationQuit(){
 		Debug.Log("Saliendo de la aplicacion");
@@ -223,25 +163,37 @@ public class PlayerController : MonoBehaviour {
 
     }
 	
-	void IniciaUser(){
-		this.userGenerator=new UserGenerator(this.context);
-		this.skeletonCapability=this.userGenerator.SkeletonCapability;
-		this.poseDetectionCapability=this.userGenerator.PoseDetectionCapability;
-		this.calibPose=this.skeletonCapability.CalibrationPose;
-		//Agregas los handlers
-		this.userGenerator.NewUser+=userGenerator_NewUser;
-		this.userGenerator.LostUser+=userGenerator_LostUser;
-		this.poseDetectionCapability.PoseDetected+=poseDetectionCapability_PoseDetected;
-		this.skeletonCapability.CalibrationComplete+=skeletonCapability_CalibrationComplete;
-		//Activar los joints depende del profile
-		//http://openni.org/docs2/Reference/_xn_types_8h_a294999eabe6eeab319a61d3d0093b174.html#a294999eabe6eeab319a61d3d0093b174
-		this.skeletonCapability.SetSkeletonProfile(SkeletonProfile.All);
-		this.joints=new Dictionary<int,Dictionary<SkeletonJoint,SkeletonJointPosition>>();
-		
+	
+	
+	void userGenerator_NewUser(object sender, NewUserEventArgs e){
+          if (this.skeletonCapability.DoesNeedPoseForCalibration){
+            	this.poseDetectionCapability.StartPoseDetection(this.calibPose, e.ID);
+           }else{
+            	this.skeletonCapability.RequestCalibration(e.ID, true);
+            }
+    }
+	
+	void poseDetectionCapability_PoseDetected(object sender, PoseDetectedEventArgs e){
+            this.poseDetectionCapability.StopPoseDetection(e.ID);
+            this.skeletonCapability.RequestCalibration(e.ID, true);
+    }
+	
+	void skeletonCapability_CalibrationComplete(object sender, CalibrationProgressEventArgs e){
+            if (e.Status == CalibrationStatus.OK){
+                this.skeletonCapability.StartTracking(e.ID);
+                this.joints.Add(e.ID, new Dictionary<SkeletonJoint, SkeletonJointPosition>());
+            }else if (e.Status != CalibrationStatus.ManualAbort){
+                if (this.skeletonCapability.DoesNeedPoseForCalibration){
+                    this.poseDetectionCapability.StartPoseDetection(calibPose, e.ID);
+                }else{
+                    this.skeletonCapability.RequestCalibration(e.ID, true);
+                }
+            }
+    }
+	
+	void userGenerator_LostUser(object sender, UserLostEventArgs e){
+		this.joints.Remove(e.ID);
 	}
 	
-	void IniciaHand(){
-		
-		
-	}
+	
 }
